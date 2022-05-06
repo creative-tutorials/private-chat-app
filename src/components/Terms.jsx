85ec11477eabdd7ab8540670e3d26f9903ef6f23
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-05-06 10:47:36 +0100

    hide privacy items

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -5,13 +5,13 @@ import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 import ChatApp from "./Chat"
 const TermsPage = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyAmyMNiqlvOL1ebeCCnHWg6bSr3qnvtIAI",
-    authDomain: "tripewebapp.firebaseapp.com",
-    databaseURL: "https://tripewebapp-default-rtdb.firebaseio.com",
-    projectId: "tripewebapp",
-    storageBucket: "tripewebapp.appspot.com",
-    messagingSenderId: "754918924840",
-    appId: "1:754918924840:web:f07b214b0d32d1ae808b1d",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
