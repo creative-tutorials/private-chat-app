e7cb1e2d0c439baaf8d32e6cf58af35ed0278442
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-05-06 10:57:50 +0100

    removed merged data

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,7 +6,6 @@ import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 
 const Chat = (props) => {
   const firebaseConfig = {
-<<<<<<< HEAD
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -14,15 +13,6 @@ const Chat = (props) => {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-=======
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
->>>>>>> 0fd1d4ef89aa5799bca37a4707e746ac345e1df7
   };
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
